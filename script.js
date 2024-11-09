d5ddbec7f233b81e75add56bc4f784485f7e7213
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-09 14:19:36 +0100

    Create getHumanChoice

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const options = ["rock", "paper", "scissors"];
 
+function getHumanChoice() {
+    let validatedInput = false;
+    while (validatedInput === false) {
+        const choice = prompt("Rock, Paper, Scissors");
+        if (choice === null) {
+            continue;
+        }
+
+        const choiceInLower = choice.toLowerCase();
+
+        if (options.includes(choiceInLower)) {
+            validatedInput = true;
+            return choiceInLower;
+        }
+    }
+}
+
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
