ef1b8f69069484d0bc819d3d49cbe67df039924f
Jiwoo Choi <jiwooch13@gmail.com>
2024-11-09 13:11:06 +0100

    Create the playRound function

--- script.js
+++ script.js
@@ -3,6 +3,34 @@ const options = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     console.log(choice);
+    return choice;
 }
 
-getComputerChoice();
+function checkWinner(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return "Tie";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") ||
+        (humanChoice === "paper" && computerChoice === "rock") ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+        return "Player";
+    } else {
+        return "Computer";
+    }
+}
+
+function playRound(humanChoice, computerChoice) {
+    const result = checkWinner(humanChoice, computerChoice);
+    if (result === "Tie") {
+        return "It's a Tie!";
+    } else if (result === "Player") {
+        return `You Win! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        return `You Lose! ${computerChoice} beats ${humanChoice}`;
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
